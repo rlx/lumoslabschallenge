004ecab6410a49dfb1384f4474d9dc90eb7a3ae0
liorrudnik <rudnik.lior@gmail.com>
2016-12-03 11:22:23 +0200

    Initial Commit

---
+++ Lumoslabschallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lumoslabschallenge
+//
+//  Created by Lior Rudnik on 12/3/16.
+//  Copyright © 2016 Lior Rudnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
